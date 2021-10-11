574183437a8cc000c96f6c236905e667c727d30c
MauricioPG <mauricio_guidini@msn.com>
2021-10-11 14:59:04 -0300

    Produção

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import Pagination from "./Pagination";
 import './styles.css';
 import { RecordsResponse } from "./types";
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-mauriciopg.herokuapp.com';
 
 const Records = () => {
 
